4538797d45f1de68d14b6f6211a9c59c7843144a
mgjeon <mgjeon@khu.ac.kr>
2025-02-16 15:08:32 +0900

    fix bug - 0.0.4

--- heliodash/app.py
+++ heliodash/app.py
@@ -1,43 +1,47 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Heliosphere Dashboard (HelioDash)",
-    page_icon="☀️",
-)
 
-
-def home():
-    st.markdown(
-        """
-        # Heliosphere Dashboard (HelioDash)
-
-        Heliosphere Dashboard (HelioDash) is an open-source tool for visualizing heliosphere-related data.
-
-        ## Available Pages
-        - [GOES](/GOES)
-        - [Juno](/Juno)
-
-    """
+def main():
+    st.set_page_config(
+        page_title="Heliosphere Dashboard (HelioDash)",
+        page_icon="☀️",
     )
 
+    def home():
+        st.markdown(
+            """
+            # Heliosphere Dashboard (HelioDash)
 
-pg = st.navigation(
-    {
-        "Overview": [
-            # Load pages from functions
-            st.Page(
-                home,
-                title="Home",
-                default=True,
-                icon=":material/home:",
-                url_path="",
-            ),
-        ],
-        "Dashboard": [
-            st.Page("dashboard/1_GOES.py", title="GOES"),
-            st.Page("dashboard/2_Juno.py", title="Juno"),
-        ],
-    }
-)
+            Heliosphere Dashboard (HelioDash) is an open-source tool for visualizing heliosphere-related data.
 
-pg.run()
+            ## Available Pages
+            - [GOES](/GOES)
+            - [Juno](/Juno)
+
+        """
+        )
+
+    pg = st.navigation(
+        {
+            "Overview": [
+                # Load pages from functions
+                st.Page(
+                    home,
+                    title="Home",
+                    default=True,
+                    icon=":material/home:",
+                    url_path="",
+                ),
+            ],
+            "Dashboard": [
+                st.Page("dashboard/1_GOES.py", title="GOES"),
+                st.Page("dashboard/2_Juno.py", title="Juno"),
+            ],
+        }
+    )
+
+    pg.run()
+
+
+if __name__ == "__main__":
+    main()
