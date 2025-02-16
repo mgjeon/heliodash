41976377cfbe36ca94af5ddf82729b4b2f2593c2
mgjeon <mgjeon@khu.ac.kr>
2025-02-16 14:24:15 +0900

    edit for PyPI packages

--- setup.py
+++ setup.py
@@ -3,5 +3,20 @@ from setuptools import find_packages, setup
 setup(
     name="heliodash",
     version="0.0.1",
+    description="Heliosphere Dashboard (HelioDash) is an open-source tool for visualizing heliosphere-related data",
+    author="Mingyu Jeon",
+    author_email="mgjeon1999@gmail.com",
+    url="https://github.com/mgjeon/heliodash",
+    install_requires=[
+        "streamlit",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "astropy[all]",
+        "sunpy[all]",
+        "ply",
+        "beautifulsoup4",
+    ],
     packages=find_packages(),
+    python_requires=">=3.10",
 )
